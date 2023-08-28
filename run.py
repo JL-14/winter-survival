24d69ae7645c89a65e6ab164eb27aa9a21612a2e
Jorgen Lovbakke <130441202+JL-14@users.noreply.github.com>
2023-08-28 12:13:07 +0100

    Add validation of start game input

--- run.py
+++ run.py
@@ -13,17 +13,36 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('winter-survival-game-content')
 
-print("Welcome to the Winter Survival Exercise!\n\nIn this exercise you will test your knowledge of survival techniques and science to see if you could survive in an extreme winter environment.\n")
+def start_screen():
+    """
+    Open start screen
+    """
+    print("Welcome to the Winter Survival Exercise!\n\nIn this exercise you will test your knowledge of survival techniques and science to see if you could survive in an extreme winter environment. Press 'esc' at any point to quit.\n")
+
+
+start_screen()
+
 name = input("Enter your name:\n")
 begin = input("Are you ready to start? y/n \n")
 
-
+def scenario():
+    """
+    Display the winter survival scenario
+    """ 
+    print(f"{name}, it is mid-January and your light airplane crashed in Northern Canada this morning at 1132am.\nYou are the only survivor.\nYou are in a wilderness area made up of thick woods broken by many lakes and rivers. The last weather report indicated that the temperature would reach minus twenty-five degrees in the daytime and minus forty at night.\nYou are dressed in business clothes as you were on your way to a meeting —suit, street shoes, and overcoat.\nWhile escaping from the plane you saw twelve items, but you can only manage to take five out of the plane wreck. Your task is to select five of the items and rank these five items according to their importance for your survival.\nThe items are:\n-A ball of steel wool\n-A small axe\n-A loaded .45-caliber pistol\n-Can of Crisco shortening\n-A newspaper\n-Cigarette lighter (without fluid)\n-Extra shirt and trousers\n-20 x 20 ft. piece of heavy-duty canvas\n-An air map made of plastic\n-Half a bottle of 85-proof whiskey\n-A compass\n-Family-size chocolate bar\n")
 
 def validate_begin(begin):
     """
     Validate the entry of y or n to begin game
     Check for blank entries, incorrect letters, non-string entries
     """
+    if begin.lower() == 'y':
+        scenario()
+    elif begin.lower() == 'n':
+        start_screen()
+    else:
+        print("Please enter y for yes, or n for no")
+
+validate_begin(begin)
 
 
-scenario = f"{name}, it is mid-January and your light airplane crashed in Northern Canada this morning at 1132am.\nYou are the only survivor.\nYou are in a wilderness area made up of thick woods broken by many lakes and rivers. The last weather report indicated that the temperature would reach minus twenty-five degrees in the daytime and minus forty at night.\nYou are dressed in business clothes as you were on your way to a meeting —suit, street shoes, and overcoat./nWhile escaping from the plane you saw twelve items, but you can only manage to take five out of the plane wreck. Your task is to select five of the items and rank these five items according to their importance for your survival.\nThe items are:\n-A ball of steel wool/n-A small axe\n-A loaded .45-caliber pistol\n-Can of Crisco shortening\n-A newspapers\nCigarette lighter (without fluid)\nExtra shirt and trousers\n-20 x 20 ft. piece of heavy-duty canvas\n-An air map made of plastic\n-Half a bottle of 85-proof whiskey\n-A compass\n-Family-size chocolate bar\n")
