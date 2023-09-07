951c07f8a1fc635ff949d03b1177b7c8cbe5583b
Jorgen Lovbakke <130441202+JL-14@users.noreply.github.com>
2023-09-07 17:19:16 +0100

    Add reworked code into functions and main()

--- run.py
+++ run.py
@@ -1,262 +1,157 @@
-"""Import gspread and Google Sheets link"""
+# Import packages for the programme
 import sys
-import keyboard
 import gspread
 from google.oauth2.service_account import Credentials
 from tabulate import tabulate
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
+def authenticate_gspread():
+    """ Set authentication parameters """
+    SCOPE = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('winter-survival-game-content')
+    CREDS = Credentials.from_service_account_file('creds.json')
+    SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+    GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+    return GSPREAD_CLIENT.open('winter-survival-game-content')
 
-# Intro to game
-intro = SHEET.worksheet('data').acell('A2').value
-print(intro)
-print(input("Are you ready to begin? y/n \n"))
+def print_intro():
+    """ Print intro """
+    intro = authenticate_gspread().worksheet('data').acell('A2').value
+    print(intro)
+    input("Press Enter to proceed\n")
 
-# Exit game command with 'esc' key
-# !!! Does not yet work !!!
-def exit_program():
-    """ Exiting programme when Esc is pressed """
-    keyboard.add_hotkey('esc', activate_exit)
-    keyboard.wait('esc')
+def print_scenario():
+    """ Print_scenario """
+    scenario = authenticate_gspread().worksheet('data').acell('B2').value
+    print(scenario)
+    input("Are you ready to select your items? y/n \n")
 
-def activate_exit():
-    print("Exiting the programme, thanks for playing!")
-    keyboard.unhook_all()
-    sys.exit()
+def select_items():
+    """ Import items from spreadsheet """
+    SHEET = authenticate_gspread()
+    data = SHEET.worksheet('data').get('B5:C16')
+    col_names = ["Item no.", "Item"]
+    print(tabulate(data, headers=col_names, tablefmt="grid"))
 
-# exit_program()
+def get_user_choices():
+    """ Get user item choices """
+    first_choice = input("\nWhich item would be your first choice? 1-12?\n")
+    second_choice = input("Which item would be your second choice? 1-12?\n")
+    third_choice = input("Which item would be your third choice? 1-12?\n")
+    fourth_choice = input("Which item would be your fourth choice? 1-12?\n")
+    fifth_choice = input("Which item would be your fifth choice? 1-12?\n")
+    return first_choice, second_choice, third_choice, fourth_choice, fifth_choice
 
-# Scenario section
-scenario = SHEET.worksheet('data').acell('B2').value
-
-print(scenario)
-
-# Importing items list
-data = SHEET.worksheet('data').get('B5:C16')
-
-# Naming columns for the items table
-col_names = ["Item no.", "Item"]
-
-print(input("Are you ready to select your items? y/n \n"))
-
-# Continue input question with validation
-# !!! Does not yet work !!!
-# while True:
-#     try:
-#         items = str(input("Are you ready to select your items? y/n \n"))
-#         if items == 'y':
-#             print(data)
-#         elif items == 'n':
-#             print(scenario)
-#             print(items)
-#         else:
-#             print("Please enter 'y' or 'n'")
-#     except ValueError:
-#         print("Please enter 'y' or 'n'")
-#     else:
-#         break
-
-# Displaying the table
-print(tabulate(data, headers=col_names, tablefmt="grid"))
-
-first_choice = input("\nWhich item would be your first choice? 1-12?\n")
-second_choice = input("Which item would be your second choice? 1-12?\n")
-third_choice = input("Which item would be your third choice? 1-12?\n")
-fourth_choice = input("Which item would be your fourth choice? 1-12?\n")
-fifth_choice = input("Which item would be your fifth choice? 1-12?\n")
-
-# Create dictionary of items
-item_descriptions = {
-    '1': "A ball of steel wool",
-    '2': "A small axe",
-    '3': "A loaded .45-caliber pistol",
-    '4': "Tin of coconut oil",
-    '5': "A newspaper",
-    '6': "Cigarette lighter (without fluid)",
-    '7': "Extra shirt and trousers",
-    '8': "A 20 x 20 ft. piece of heavy-duty canvas",
-    '9': "A sectional air map made of plastic",
-    '10': "Half a bottle of 85-proof whisky",
-    '11': "A compass",
-    '12': "A family-size chocolate bar"
-}
-
-# Create dictionary of expert views
-expert_view = {
-    1: "Cigarette lighter (without fluid)",
-    2: "A ball of steel wool",
-    3: "Extra shirt and trousers",
-    4: "Tin of coconut oil",
-    5: "A 20 x 20 ft. piece of heavy-duty canvas",
-    6: "A small axe",
-    7: "A family-size chocolate bar",
-    8: "A newspaper",
-    9: "A loaded .45-caliber pistol",
-    10: "Half a bottle of 85-proof whisky",
-    11: "A compass",
-    12: "A sectional air map made of plastic" 
-}
-
-# Matching item description to item choice
-def get_item_description(choice):
-    """
-    Function to get item description based on choice
-    """
+def get_item_description(choice, item_descriptions):
+    """ Get description of items chosen """
     return item_descriptions.get(choice, "")
 
-first_item = get_item_description(first_choice)
-second_item = get_item_description(second_choice)
-third_item = get_item_description(third_choice)
-fourth_item = get_item_description(fourth_choice)
-fifth_item = get_item_description(fifth_choice)
+def calculate_score(choices, expert_view):
+    """ Calculate scores based on user choice and expert ranking """
+    score = 0
+    for i, choice in enumerate(choices, 1):
+        matching_expert_key = None
+        for key, value in expert_view.items():
+            if value == choice:
+                matching_expert_key = key
+                break
+        if matching_expert_key is not None:
+            score += abs(matching_expert_key - i)
+    return score
 
-print(f"""\nYou have chosen: 
-      1. {first_item}
-      2. {second_item}
-      3. {third_item}
-      4. {fourth_item}
-      5. {fifth_item}
-    """)
+def display_score(score):
+    """ Display final score """
+    if score <= 5:
+        return "Excellent! You have a very good chance of survival!"
+    elif 6 <= score <= 12:
+        return "Very Good! You have a pretty good chance of survival!"
+    elif 13 <= score <= 20:
+        return "Good. You have a reasonable chance of survival."
+    else:
+        return "Not So Good... You have a pretty low chance of survival based on the items chosen."
 
-choice_confirm = input("Are you happy with your choices? y/n \n")
+def get_feedback(SHEET, choices):
+    " Import expert feedback for items chosen "
+    feedback = []
+    for choice in choices:
+        feedback.append(SHEET.worksheet("feedback").cell(int(choice), 2).value)
+    return feedback
 
-# Convert first_choice to integer for calculating score
-first_choice = int(first_choice)
-second_choice = int(second_choice)
-third_choice = int(third_choice)
-fourth_choice = int(fourth_choice)
-fifth_choice = int(fifth_choice)
+def main():
+    """ Run programme """
+    print_intro()
 
-# First item score
-matching_expert_key = None
+    print_scenario()
 
-for key, value in expert_view.items():
-    if value == first_item:
-        matching_expert_key = key
-        break
+    select_items()
 
-score_one = 0
-if matching_expert_key is not None:
-    score_one = matching_expert_key - 1
-    score_one = abs(score_one)
-else: 
-    print("No match found")
+    first_choice, second_choice, third_choice, fourth_choice, fifth_choice = get_user_choices()
 
-# Second item score
-matching_expert_key_two = None
+    item_descriptions = {
+        '1': "A ball of steel wool",
+        '2': "A small axe",
+        '3': "A loaded .45-caliber pistol",
+        '4': "Tin of coconut oil",
+        '5': "A newspaper",
+        '6': "Cigarette lighter (without fluid)",
+        '7': "Extra shirt and trousers",
+        '8': "A 20 x 20 ft. piece of heavy-duty canvas",
+        '9': "A sectional air map made of plastic",
+        '10': "Half a bottle of 85-proof whisky",
+        '11': "A compass",
+        '12': "A family-size chocolate bar"
+    }
 
-for key, value in expert_view.items():
-    if value == second_item:
-        matching_expert_key_two = key
-        break
+    expert_view = {
+        1: "Cigarette lighter (without fluid)",
+        2: "A ball of steel wool",
+        3: "Extra shirt and trousers",
+        4: "Tin of coconut oil",
+        5: "A 20 x 20 ft. piece of heavy-duty canvas",
+        6: "A small axe",
+        7: "A family-size chocolate bar",
+        8: "A newspaper",
+        9: "A loaded .45-caliber pistol",
+        10: "Half a bottle of 85-proof whisky",
+        11: "A compass",
+        12: "A sectional air map made of plastic"
+    }
 
-score_two = 0
-if matching_expert_key_two is not None:
-    score_two = matching_expert_key_two - 2
-    score_two = abs(score_two)
-else: 
-    print("No match found")
+    choices = [first_choice, second_choice, third_choice, fourth_choice, fifth_choice]
 
-# Third item score
-matching_expert_key_three = None
+    print(f"\nYou have chosen:\n")
+    for i, choice in enumerate(choices, 1):
+        item_description = get_item_description(choice, item_descriptions)
+        print(f"{i}. {item_description}")
 
-for key, value in expert_view.items():
-    if value == third_item:
-        matching_expert_key_three = key
-        break
+    choice_confirm = input("\nAre you happy with your choices? y/n \n")
 
-score_three = 0
-if matching_expert_key_three is not None:
-    score_three = matching_expert_key_three - 3
-    score_three = abs(score_three)
-else: 
-    print("No match found")
+    total_score = calculate_score(choices, expert_view)
 
-# Fourth item score
-matching_expert_key_four = None
+    print(f"\nYour final score is:\n\n{total_score}\n\nwhich is {display_score(total_score)}\n")
 
-for key, value in expert_view.items():
-    if value == fourth_item:
-        matching_expert_key_four = key
-        break
+    feedback_choice = input("Would you like to see the survival expert's feedback on the items you chose? y/n \n")
 
-score_four = 0
-if matching_expert_key_four is not None:
-    score_four = matching_expert_key_four - 4
-    score_four = abs(score_four)
-else: 
-    print("No match found")
+    if feedback_choice.lower() == 'y':
+        feedback = get_feedback(authenticate_gspread(), choices)
+        print("\nThe expert's view on your choices were:\n")
+        for i, (choice, item_feedback) in enumerate(zip(choices, feedback), 1):
+            item_description = get_item_description(choice, item_descriptions)
+            print(f"{i}. {item_description}:\n{item_feedback}\n")
 
-# Fifth item score
-matching_expert_key_five = None
+    print("Do you want to try again, see the expert's item rankings, or quit?")
+    choice = input("Type 't' to try again/ 'e' to see expert rankings/ 'q' to quit\n")
 
-for key, value in expert_view.items():
-    if value == fifth_item:
-        matching_expert_key_five = key
-        break
+    if choice == 'e':
+        expert_ranking_table = authenticate_gspread().worksheet("expert_rankings").get('A1:B12')
+        col_names_expert = ["Expert rank", "Item"]
+        print("\nThese are the expert's rankings of the items:\n")
+        print(tabulate(expert_ranking_table, headers=col_names_expert, tablefmt="grid"))
 
-score_five = 0
-if matching_expert_key_five is not None:
-    score_five = matching_expert_key_five - 5
-    score_five = abs(score_five)
-else: 
-    print("No match found")
+    print("\nThank you for visiting the Winter Survival Exercise!\n")
 
-# Total score
-total_score = score_one + score_two + score_three + score_four + score_five
-
-if total_score <= 5:
-    print(f"\nYour final score is:\n\n{total_score}\n\nwhich is Excellent! You have a very good chance of survival!\n")
-elif total_score >= 6 and total_score <= 12:
-    print(f"\nYour final score is:\n\n{total_score}\n\nwhich is Very Good! You have a pretty good chance of survival!\n")
-elif total_score >= 13 and total_score <= 20:
-    print(f"\nYour final score is:\n\n{total_score}\n\nwhich is Good. You have a reasonable chance of survival.\n")
-else:
-    print(f"\nYour final score is:\n\n{total_score}\n\nwhich is Not So Good... You have a pretty low chance of survival based on the items chosen.\n")
-
-print(input("Would you like to see the survival expert's feedback on the items you chose? y/n \n"))
-
-# Importing item feedback from worksheet
-print("\nThe expert's view on your choices were:\n")
-
-print(f"1. {first_item}:\n")
-feedback1 = SHEET.worksheet("feedback").cell(first_choice, 2).value
-print(f"{feedback1}\n")
-print(f"2. {second_item}:\n")
-feedback2 = SHEET.worksheet("feedback").cell(second_choice, 2).value
-print(f"{feedback2}\n")
-print(f"3. {third_item}:\n")
-feedback3 = SHEET.worksheet("feedback").cell(third_choice, 2).value
-print(f"{feedback3}\n")
-print(f"4. {fourth_item}:\n")
-feedback4 = SHEET.worksheet("feedback").cell(fourth_choice, 2).value
-print(f"{feedback4}\n")
-print(f"5. {fifth_item}:\n")
-feedback5 = SHEET.worksheet("feedback").cell(fifth_choice, 2).value
-print(f"{feedback5}\n")
-
-print("Do you want to try again, see the expert's item rankings, or quit?")
-print(input("Type 't' to try again/ 'e' to see expert rankings/ 'q' to quit\n"))
-
-# Expert ranking table
-expert_ranking_table = SHEET.worksheet("expert_rankings").get('A1:B12')
-
-# Naming columns for the items table
-col_names_expert = ["Expert rank", "Item"]
-
-# Displaying the table
-print("\nThese are the expert's rankings of the items:\n")
-print(tabulate(expert_ranking_table, headers=col_names_expert, tablefmt="grid"))
-
-# Final thank you
-print("\nThank you for visiting the Winter Survival Exercise!\n")
-
+if __name__ == "__main__":
+    main()
