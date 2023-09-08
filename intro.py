4b9710d4d36c9553a86c059957062be7e3228713
Jorgen Lovbakke <130441202+JL-14@users.noreply.github.com>
2023-09-08 18:04:39 +0100

    Add intro and scenario to standalone files

---
+++ intro.py
@@ -0,0 +1,11 @@
+intro_text = f"""
+-----------------------------------------------
+
+Welcome to the Winter Survival Exercise!
+
+In this exercise you will test your knowledge of survival techniques and science to see if you could survive in an extreme winter environment. 
+
+Press 'esc' at any point to quit.
+"""
+
+print(intro_text)
---
+++ scenario.py
@@ -0,0 +1,24 @@
+scenario_text = f"""
+-----------------------------------------------
+
+It is mid-January and your light airplane crashed in Northern Canada this morning at 1132am.
+
+You are the only survivor.
+
+You are in a snowy wilderness area made up of thick woods broken by many lakes and rivers. The last weather report indicated that the temperature will be minus twenty-five degrees in the daytime and minus forty at night.
+
+You are dressed in business clothes as you were on your way to a meeting —suit, street shoes, and overcoat.
+
+While escaping from the plane you saw twelve items, but you can only manage to take five out of the plane wreck. Your task is to select five of the items and rank these five items according to their importance for your survival (1 being the most important, 2 the second most important, and so on).
+
+Mid-January is the coldest time of the year where you are. The first problem you face, therefore, is to preserve body heat and to protect your body against heat loss. Your body will be using energy to stay warm, so that is another consideration.
+You also need to consider how you can attract the attention of search parties.
+
+-----------------------------------------------
+Scoring
+
+This exercise was created by a US Army survival expert who also ranked the items. Your 5 rankings will be compared with how he ranked the items, so if your first choice (i.e. 1) was also ranked by him as the first choice (i.e. 1), you will get 0 penalty points (expert ranking - your ranking, so 1 - 1 = 0. However, if your second choice (i.e. 2) was ranked by the expert as the 11th most important item (i.e. 11), you will get 9 penalty points (11 - 2 = 9). Your aim is to get as low a total score for the 5 items as possible. 
+
+Finally, there are no absolutely right answers, and in real life your ability to use the items chosen will affect their usefulness. You may therefore not agree with the exact rankings the expert has given, but the expert's rankings would unquestionably give you a chance of survival in this environment.
+"""
+print(scenario_text)
