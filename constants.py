fe5a24db765ba3a7b0745589218d3443a7e3ce35
Jorgen Lovbakke <130441202+JL-14@users.noreply.github.com>
2023-09-14 15:57:57 +0100

    Add paragraph numbers to scenario and scoring section, split scoring from scenario

--- constants.py
+++ constants.py
@@ -6,22 +6,24 @@ Welcome to the Winter Survival Exercise!
 In this exercise you will test your knowledge of survival techniques and science to see if you could survive in an extreme winter environment. 
 
 Press 'Ctrl + C' at any point to quit.
+
+You may at times need to scroll up to see the beginning of each section of text (or the beginning of tables in the exercise).
 """
 
 scenario_text = """
 -----------------------------------------------
 
-It is mid-January and your light airplane crashed in Northern Canada this morning at 1132am.
+1. It is mid-January and your light airplane crashed in Northern Canada this morning at 1132am.
 
-You are the only survivor.
+2. You are the only survivor.
 
-You are in a snowy wilderness area made up of thick woods broken by many lakes and rivers. The last weather report indicated that the temperature will be minus twenty-five degrees in the daytime and minus forty at night.
+3. You are in a snowy wilderness area made up of thick woods broken by many lakes and rivers. The last weather report indicated that the temperature will be minus twenty-five degrees in the daytime and minus forty at night.
 
-You are dressed in business clothes as you were on your way to a meeting —suit, street shoes, and overcoat.
+4. You are dressed in business clothes as you were on your way to a meeting —suit, street shoes, and overcoat.
 
-While escaping from the plane you saw twelve items, but you can only manage to take five out of the plane wreck. Your task is to select five of the items and rank these five items according to their importance for your survival (1 being the most important, 2 the second most important, and so on). You can change your mind about which items to take ONLY once.
+5. While escaping from the plane you saw twelve items, but you can only manage to take five out of the plane wreck. Your task is to select five of the items and rank these five items according to their importance for your survival (1 being the most important, 2 the second most important, and so on). You can change your mind about which items to take ONLY once.
 
-Mid-January is the coldest time of the year where you are. The first problem you face, therefore, is to preserve body heat and to protect your body against heat loss. Your body will be using energy to stay warm, so that is another consideration.
+6. Mid-January is the coldest time of the year where you are. The first problem you face, therefore, is to preserve body heat and to protect your body against heat loss. Your body will be using energy to stay warm, so that is another consideration.
 You also need to consider how you can attract the attention of search parties.
 """
 
@@ -30,13 +32,13 @@ score_text = """
 -----------------------------------------------
 Scoring
 
-This exercise was created by a US Army survival expert who also ranked the items. 
+1. This exercise was created by a US Army survival expert who also ranked the items. 
 
-Your 5 rankings will be compared with how he ranked the items, so if your first choice (i.e. 1) was also ranked by him as the first choice (i.e. 1), you will get 0 penalty points (expert ranking - your ranking, so 1 - 1 = 0. However, if your second choice (i.e. 2) was ranked by the expert as the 11th most important item (i.e. 11), you will get 9 penalty points (11 - 2 = 9). 
+2. Your 5 rankings will be compared with how he ranked the items, so if your first choice (i.e. 1) was also ranked by him as the first choice (i.e. 1), you will get 0 penalty points (expert ranking - your ranking, so 1 - 1 = 0. However, if your second choice (i.e. 2) was ranked by the expert as the 11th most important item (i.e. 11), you will get 9 penalty points (11 - 2 = 9). 
 
-Your aim is to get as low a total score for the 5 items as possible. 
+3. Your aim is to get as low a total score for the 5 items as possible. The best score is 0, and the poorest is 35.
 
-Finally, there are no absolutely right answers, and in real life your ability to use the items chosen will affect their usefulness. You may therefore not agree with the exact rankings the expert has given, but the expert's rankings would unquestionably give you a chance of survival in this environment.
+4. Finally, there are no absolutely right answers, and in real life your ability to use the items chosen will affect their usefulness. You may therefore not agree with the exact rankings the expert has given, but the expert's rankings would unquestionably give you a chance of survival in this environment.
 """
 
 item_descriptions = {
